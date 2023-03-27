3a76bda15efe9598cc2e313e04b2e762d16d5c09
HalenarAkos <hakos0223@gmail.com>
2023-03-27 09:18:19 +0200

    #

---
+++ SimVillage.Model/Citizen.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimVillage.Model
+{
+    internal class Citizen
+    {
+        int Age;
+        int Salary;
+
+        public int calcHappiness()
+        {
+            return 0;
+        }
+    }
+}
