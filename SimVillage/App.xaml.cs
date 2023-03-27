f1bac6f1ca59ea21a6edcb0b1e67c507a4744acf
J2868O <j2868o@inf.elte.hu>
2023-03-27 09:11:57 +0200

    testing

--- SimVillage/App.xaml.cs
+++ SimVillage/App.xaml.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿using SimVillage.Model;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Timers;
 using System.Windows;
 
 namespace SimVillage
@@ -13,5 +15,17 @@ namespace SimVillage
     /// </summary>
     public partial class App : Application
     {
+        private Timer timer;
+
+        private City city;
+
+        private SimVillage.Model.Persistence persistence;
+
+        public App()
+        {
+            InitializeComponent();
+            persistence = new SimVillage.Model.Persistence();
+            city = new City(persistence, "asd");
+        }
     }
 }
